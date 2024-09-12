284cdd637633916307ba3905422e823dfe149648
StingySpoilero <dalaujon@gmail.com>
2024-09-12 01:33:23 -0500

    ALAB308.3.1

---
+++ script.js
@@ -0,0 +1,14 @@
+const PI = 3.1415;
+const area = PI * radius * radius;
+const radius = 5 
+
+
+let number = 1;
+if (number > 0.8*(3.1415*5*5)) {
+    console.log(`growth`)
+}
+if (plants > 0.8 * ){
+
+} else if (plants){
+    
+}else 
